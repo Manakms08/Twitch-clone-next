27302549770229b71fb52b597115ba5e28afa9d4
Manakms08 <72964526+Manakms08@users.noreply.github.com>
2022-12-07 13:38:13 +0530

    New Commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -221,7 +221,7 @@ const Navbar = () => {
           <Link href='/#categories'>Top Categories</Link>
           </li>
           <li onClick={()=> setNav(false)} className='p-4 text-3xl font-bold'>
-          <Link href='/account'>Account</Link>
+          <Link href='/Account'>Account</Link>
           </li>
         </ul>
        </div>
